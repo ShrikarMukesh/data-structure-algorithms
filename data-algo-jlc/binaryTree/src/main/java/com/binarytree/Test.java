6a19b3a7d18590efe0539fb21a3cd89da68ac2f4
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2022-10-04 10:22:28 +0530

    implemented bst ds

--- data-algo-jlc/binaryTree/src/main/java/com/binarytree/Test.java
+++ data-algo-jlc/binaryTree/src/main/java/com/binarytree/Test.java
@@ -7,11 +7,16 @@ import java.util.LinkedList;
 @Slf4j
 public class Test {
     public static void main(String[] args) {
-        LinkedList<BinTreeNode> linkedList = new LinkedList<>();
-        linkedList.add(new BinTreeNode(12));
-        String str = "Shrikar";
-        linkedList.forEach(e-> log.debug("e" , str.concat(String.valueOf(e.getData()))));
-
-
+        int n = 10;
+        int count =0;
+        for(int i=2;i<n/2;i++){
+            if(n % i == 0){
+                count++;
+                return;
+            }
+        }
+        if(count == 0 ){
+            System.out.println(n + "is prime ");
+        }
     }
 }
